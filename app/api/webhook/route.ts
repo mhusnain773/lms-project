dd1b3f8de88fb4695596a6592833d179d83dc69a
mhusnain773 <itxhusnain773@gmail.com>
2023-12-28 16:11:48 +0500

    final

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,59 @@
+import Stripe from "stripe";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+import { stripe } from "@/lib/stripe";
+import { db } from "@/lib/db";
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = headers().get("stripe-signature") as string;
+  console.log(signature);
+
+  if (!signature) {
+    console.error("Stripe signature is missing in the header.");
+    return new NextResponse("Stripe signature is missing in the header.", {
+      status: 400,
+    });
+  }
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error: any) {
+    console.error(error.message);
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+  }
+
+  console.log(event);
+  const session = event.data.object as Stripe.Checkout.Session;
+  const userId = session?.metadata?.userId;
+  const courseId = session?.metadata?.courseId;
+
+  if (event.type === "checkout.session.completed") {
+    if (!userId || !courseId) {
+      return new NextResponse(`Webhook Error: Missing metadata`, {
+        status: 400,
+      });
+    }
+
+    await db.purchase.create({
+      data: {
+        courseId: courseId,
+        userId: userId,
+      },
+    });
+  } else {
+    return new NextResponse(
+      `Webhook Error: Unhandled event type ${event.type}`,
+      { status: 200 }
+    );
+  }
+
+  return new NextResponse(null, { status: 200 });
+}
